36b406ff1271d0a021a8f79e904d0736ac71cc53
Michael Mitchell-Halter (Apex Systems <v-michaelmi@microsoft.com>
2023-03-15 11:24:19 -0700

    Migrated application to use to-do's instead of making Graph API calls

---
+++ 2-Authorization/2-call-own-api-blazor-server/ToDoListClient/Pages/ToDoPages/ToDoListBase.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Abstractions;
+using ToDoListApi.Models;
+
+namespace ToDoListClient.Pages.ToDoPages
+{
+
+    public class ToDoListBase : ComponentBase
+    {
+        [Inject]
+        IDownstreamApi DownstreamApi { get; set; }
+
+        [Inject]
+        MicrosoftIdentityConsentAndConditionalAccessHandler ConsentHandler { get; set; }
+
+        [Inject]
+        NavigationManager Navigation { get; set; }
+
+        const string ServiceName = "ToDoApi";
+        protected IEnumerable<ToDo> toDoList = new List<ToDo>();
+        protected ToDo toDo = new ToDo();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetToDoListService();
+        }
+
+        /// <summary>
+        /// Gets all todo list items.
+        /// </summary>
+        /// <returns></returns>
+        [AuthorizeForScopes(ScopeKeySection = "TodoList:TodoListScope")]
+        private async Task GetToDoListService()
+        {
+            try
+            {
+                toDoList = (await DownstreamApi.GetForUserAsync<IEnumerable<ToDo>>(
+                    ServiceName,
+                    options => options.RelativePath = "/api/todo"))!;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                // Process the exception from a user challenge
+                ConsentHandler.HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the selected item then retrieves the todo list.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        protected async Task DeleteItem(int Id)
+        {
+            await DownstreamApi.DeleteForUserAsync(
+                ServiceName,
+                toDo,
+                options => options.RelativePath = $"api/todo/{Id}");
+
+            await GetToDoListService();
+        }
+    }
+}
