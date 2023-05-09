67e3e9db84518a10af086e5e34797f7040dfb7d1
salman90 <v-ssalem@microsoft.com>
2023-05-09 11:27:51 -0700

    omit openid scope from sample

--- 1-Authentication/4-sign-in-device-code/Program.cs
+++ 1-Authentication/4-sign-in-device-code/Program.cs
@@ -6,13 +6,14 @@ var configuration =  new ConfigurationBuilder()
             
 var config = configuration.Build();
 var publicClientOptions = config.GetSection("AzureAd");
+var scopes = new string[] { };
 
 var app = PublicClientApplicationBuilder.Create(publicClientOptions.GetValue<string>("ClientId"))
     .WithAuthority(publicClientOptions.GetValue<string>("Authority"))
     .WithExtraQueryParameters("dc=ESTS-PUB-EUS-AZ1-FD000-TEST1")
     .Build();
 
-var result = await app.AcquireTokenWithDeviceCode(new [] { "openid" }, async deviceCode => {
+var result = await app.AcquireTokenWithDeviceCode(scopes, async deviceCode => {
     Console.WriteLine($"In a broswer, navigate to the URL '{deviceCode.VerificationUrl}' and enter the code '{deviceCode.UserCode}'");
     await Task.FromResult(0);
 }).ExecuteAsync();
